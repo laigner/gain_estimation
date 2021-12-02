02358d34c05766156d6321faa6be07eefd469198
Lukas Aigner <aigner.lukas@icloud.com>
2021-12-02 15:32:10 +0100

    kinda works

--- gain_estimation.py
+++ gain_estimation.py
@@ -4,17 +4,25 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 class GainEstimation:
-
-    def __init__(self, losses_pump: float = 1, losses_sled: float = 0.5, mode_size_z:
-    float = 0.7, mode_size_y: float = 0.7, length_min: float = 0.001, length_max:
-    float = 50, number_of_modes: int = 1, initial_pump_power: float = 0.,
-                 power_overlap_factor: float = 1, amp_bandwidth_factor: float = 3.2,
-                 bandwidth_overlap_sled_factor: float = 5.7):
+    def __init__(
+        self,
+        losses_pump: float = 1,
+        losses_sled: float = 0,
+        mode_size_z: float = 0.7,
+        mode_size_y: float = 0.7,
+        length_min: float = 0.001,
+        length_max: float = 500,
+        number_of_modes: int = 1,
+        initial_pump_power: float = 0.0,
+        power_overlap_factor: float = 1,
+        amp_bandwidth_factor: float = 3.2,
+        bandwidth_overlap_sled_factor: float = 5.7,
+    ):
         """Initialize class object.
-        
-        Initialize the object e.g. test_object = GainEstimation(*args). If called 
+
+        Initialize the object e.g. test_object = GainEstimation(*args). If called
         without arguments, default values will be used.
-        
+
         Parameters
         ----------
         losses_pump: float
@@ -26,7 +34,7 @@ class GainEstimation:
         mode_size_y: float
             size of the mode in y direction in um, default is 0.7 um
         length_min: float
-            minimum length of the crystal in mm, default is 0.001 mm 
+            minimum length of the crystal in mm, default is 0.001 mm
         length_max: float
             maximum length of crystal in mm, default is 50 mm
         number_of_modes: int
@@ -45,8 +53,9 @@ class GainEstimation:
         self.mode_size_z = mode_size_z
         self.mode_size_y = mode_size_y
         self.initial_pump_power = initial_pump_power
-        self.pump_intensity = self.initial_pump_power / (self.mode_size_z *
-                                                         self.mode_size_y)
+        self.pump_intensity = self.initial_pump_power / (
+            self.mode_size_z * self.mode_size_y
+        )
         self.intensity_factor = 2.6 * 3.6 / (self.mode_size_z * self.mode_size_y)
         self.gain_inside = 0.00336 * np.sqrt(self.intensity_factor)
         self.length_min = length_min
@@ -57,13 +66,22 @@ class GainEstimation:
         self.bandwidth_overlap_sled_factor = bandwidth_overlap_sled_factor
         self.length_array = np.arange(length_min, length_max)
         self.amp_bandwidth = self.amp_bandwidth_factor / self.length_array
-        self.bandwidth_overlap_sled = [min(x / self.bandwidth_overlap_sled_factor,
-                                           1) for x in self.amp_bandwidth]
-        self.power_overlap = self.power_overlap_factor * \
-                             np.array(self.bandwidth_overlap_sled)
+        self.bandwidth_overlap_sled = [
+            min(x / self.bandwidth_overlap_sled_factor, 1) for x in self.amp_bandwidth
+        ]
+        self.power_overlap = self.power_overlap_factor * np.array(
+            self.bandwidth_overlap_sled
+        )
 
     def calculate_pump_power_per_mode(self, length, loss) -> float:
         """Calculate the pump power per mode at length
+        Parameters
+        ----------
+        length: float
+            specific length of crystal
+
+        loss: float
+            loss of pump power
 
         Returns
         -------
@@ -75,17 +93,39 @@ class GainEstimation:
             pump_power_per_mode = initial_pump_power
         else:
             pump_power_per_mode = 10 ** (
-                    -(length - self.length_min) * self.losses_pump /
-                    100) * initial_pump_power - loss
+                -(length - self.length_min) * self.losses_pump / 100
+            ) * initial_pump_power - abs(loss)
         return pump_power_per_mode
 
-    def calculate_gain_per_mode(self, pump_power_per_mode, length, index):
+    def calculate_gain_per_mode(
+        self, pump_power_per_mode: float, length: float, index: int
+    ) -> float:
+        """Calculate gain per mode at specific length
+
+        Parameters
+        ----------
+        pump_power_per_mode: float
+            pump power per mode at specific length
+        length: float
+            specific length of crystal
+        index: int
+            index of length array
+
+        Returns
+        -------
+        gain_per_mode: float
+            gain per mode at specific length
+        """
         if length == self.length_min:
-            gain_per_mode = np.sqrt(pump_power_per_mode) * self.gain_inside * \
-                            self.length_min
+            gain_per_mode = (
+                np.sqrt(pump_power_per_mode) * self.gain_inside * self.length_min
+            )
         else:
-            gain_per_mode = np.sqrt(pump_power_per_mode) * self.gain_inside * (
-                    length - self.length_array[index - 1])
+            gain_per_mode = (
+                np.sqrt(pump_power_per_mode)
+                * self.gain_inside
+                * (length - self.length_array[index - 1])
+            )
         return gain_per_mode
 
     def calculate_losses_in_amp_band(self, gain_per_mode, length, old) -> float:
@@ -99,8 +139,9 @@ class GainEstimation:
         if length == self.length_min:
             losses_band = np.sinh(gain_per_mode) ** 2 + 1
         else:
-            losses_band = (np.sinh(gain_per_mode + old) ** 2 + 1) * 10 ** (-(
-                    length - self.length_min) * self.losses_sled / 100)
+            losses_band = (np.sinh(gain_per_mode + old) ** 2 + 1) * 10 ** (
+                -(length - self.length_min) * self.losses_sled / 100
+            )
         return losses_band
 
     def calculate_power_after_crystal(self, losses_band, length, index) -> float:
@@ -115,87 +156,100 @@ class GainEstimation:
             power_after_crystal = losses_band * self.power_overlap_factor
         else:
             power_after_crystal = losses_band * self.power_overlap[index] + (
-                    self.power_overlap_factor - self.power_overlap[index]) * 10 ** (
-                                          -(length - self.length_min) *
-                                          self.losses_sled / 100)
+                self.power_overlap_factor - self.power_overlap[index]
+            ) * 10 ** (-(length - self.length_min) * self.losses_sled / 100)
 
         return power_after_crystal
 
     def get_power_after_crystal_list(self) -> list:
-        power_result = []
-        old_gain = 0
-        loss = 0
-        pump = []
-        for index, length in enumerate(self.length_array):
-            pump_power_per_mode = self.calculate_pump_power_per_mode(
-                length=length,
-                loss=loss)
-            print(pump_power_per_mode)
-            gain_per_mode = self.calculate_gain_per_mode(
-                pump_power_per_mode=pump_power_per_mode, length=length, index=index)
-            losses_band = self.calculate_losses_in_amp_band(
-                gain_per_mode=gain_per_mode,
-                length=length, old=old_gain)
-            old_gain += gain_per_mode
-            power_after_crystal = self.calculate_power_after_crystal(
-                losses_band=losses_band,
-                length=length,
-                index=index)
-            power_result.append(power_after_crystal)
-            if length == self.length_min:
-                loss = 0.1
-            elif index == 1:
-                loss = power_after_crystal - power_result[-1]
-            else:
-                loss = power_after_crystal - power_result[-2]
-                print(loss)
-        return power_result
-
-    def plot_amp_loss_surface(self, modes: list = [1, 3],
-                              powers: list = np.linspace(0.1, 200, num=50),
-                              y_lim: list = [-1, 30], x_lim: list = [0, 50]):
-        """Plotting the power after the crystal over input power and crystal length
-        as surface plot for different modes
-
-        Parameters
-        ----------
-        modes: list
-            contains the specific numbers of modes to analyse
-        powers: list
-            contains the different powers to analyse at
-        y_lim: list
-            contains minimum and maximum y value for plotting
-        x_lim: list
-            contains minimum and maximum x value for plotting
+        """returns a list of the power after the crystal and the loss after the crystal
 
         Returns
         -------
 
         """
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        x, y = np.meshgrid(self.length_array, np.linspace(start=0.1,
-                                                          stop=200, num=50))
-        for mode in modes:
-            z = []
-            self.number_of_modes = mode
-            for power in powers:
-                self.initial_pump_power = power
-                z.append(self.get_power_after_crystal_list())
+        power_result = []
+        old_gain = 0
+        loss = 0
+        pump_loss = []
+        for index, length in enumerate(self.length_array):
+            # calculate pump power per mode
+            pump_power_per_mode = self.calculate_pump_power_per_mode(
+                length=length, loss=loss
+            )
+            # avoid negative values
+            if pump_power_per_mode < 0:
+                pump_power_per_mode = 0
+            # calculate gain per mode
+            gain_per_mode = self.calculate_gain_per_mode(
+                pump_power_per_mode=pump_power_per_mode, length=length, index=index
+            )
+            # sum old gain per mode with new gain per mode
+            old_gain += gain_per_mode
+            # calculate losses in amp band
+            losses_band = self.calculate_losses_in_amp_band(
+                gain_per_mode=gain_per_mode, length=length, old=old_gain
+            )
+            # calculate power after crystal
+            power_after_crystal = self.calculate_power_after_crystal(
+                losses_band=losses_band, length=length, index=index
+            )
+            # define initial loss
+            if length == self.length_min:
+                loss = 0.01
+            # calculate loss (difference of power after crystal values)
+            else:
+                loss = (power_after_crystal - power_result[-1]) * 100
+            if loss > self.initial_pump_power:
+                loss = self.initial_pump_power
+            power_result.append(power_after_crystal)
+            pump_loss.append(loss)
+        return power_result, pump_loss
 
-            c = ax.plot_surface(x, y, np.array(z), label=f'#Modes: {mode}')
-            c._facecolors2d = c._facecolor3d
-            c._edgecolors2d = c._edgecolor3d
-        plt.legend()
-        plt.xlabel('Length [mm]')
-        plt.ylabel('Input power [mW]')
-        ax.set_zlabel('Power [mW]')
-        fig.savefig('amp_loss_surface.pdf')
-        plt.show()
+    # def plot_amp_loss_surface(self, modes: list = [1, 3],
+    #                           powers: list = np.linspace(0.1, 200, num=50),
+    #                           y_lim: list = [-1, 30], x_lim: list = [0, 50]):
+    #     """Plotting the power after the crystal over input power and crystal length
+    #     as surface plot for different modes
+    #
+    #     Parameters
+    #     ----------
+    #     modes: list
+    #         contains the specific numbers of modes to analyse
+    #     powers: list
+    #         contains the different powers to analyse at
+    #     y_lim: list
+    #         contains minimum and maximum y value for plotting
+    #     x_lim: list
+    #         contains minimum and maximum x value for plotting
+    #
+    #     Returns
+    #     -------
+    #
+    #     """
+    #     #create figure
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(111, projection='3d')
+    #     x, y = np.meshgrid(self.length_array, np.linspace(start=0.1,
+    #                                                       stop=200, num=50))
+    #     for mode in modes:
+    #         z = []
+    #         self.number_of_modes = mode
+    #         for power in powers:
+    #             self.initial_pump_power = power
+    #             z.append(self.get_power_after_crystal_list())
+    #
+    #         c = ax.plot_surface(x, y, np.array(z), label=f'#Modes: {mode}')
+    #         c._facecolors2d = c._facecolor3d
+    #         c._edgecolors2d = c._edgecolor3d
+    #     plt.legend()
+    #     plt.xlabel('Length [mm]')
+    #     plt.ylabel('Input power [mW]')
+    #     ax.set_zlabel('Power [mW]')
+    #     fig.savefig('amp_loss_surface.pdf')
+    #     plt.show()
 
-    def plot_amp_loss(self, modes: list = [1, 3],
-                      powers: list = [0.1, 50, 100],
-                      y_lim: list = [-1, 30], x_lim: list = [0, 50]):
+    def plot_amp_loss(self, modes: list = [1], powers: list = [100]):
         """Plot power after crystal over crystal length for different number of modes
          as line plots
 
@@ -214,17 +268,39 @@ class GainEstimation:
         -------
 
         """
+        # create figure
+        fig = plt.figure()
+        # iterate through input values
+        for mode in modes:
+            self.number_of_modes = mode
+            for power in powers:
+                self.initial_pump_power = power
+                # plot the results
+                plt.plot(
+                    self.length_array,
+                    self.get_power_after_crystal_list()[0],
+                    label=f"{power} mW, #Modes: {mode}",
+                )
+        plt.legend()
+        plt.ylim(0, powers[-1])
+        plt.xlim(self.length_min, self.length_max)
+        plt.xlabel("Length [mm]")
+        plt.ylabel("Power [mW]")
+        # save figure
+        fig.savefig("amp_loss.pdf")
+        plt.show()
+
+    def plot_loss_pump(self, modes: list = [1], powers: list = [100]):
         fig = plt.figure()
         for mode in modes:
             self.number_of_modes = mode
             for power in powers:
                 self.initial_pump_power = power
-                plt.plot(self.length_array, self.get_power_after_crystal_list(),
-                         label=f'{power} mW, #Modes: {mode}')
+                plt.plot(self.length_array, self.get_power_after_crystal_list()[1])
         plt.legend()
-        plt.ylim(y_lim[0], y_lim[1])
-        plt.xlim(x_lim[0], x_lim[1])
-        plt.xlabel('Length [mm]')
-        plt.ylabel('Power [mW]')
-        fig.savefig('amp_loss.pdf')
+        plt.ylim(0, powers[-1])
+        plt.xlim(self.length_min, self.length_max)
+        plt.xlabel("Length [mm]")
+        plt.ylabel("Loss [mW]")
+        fig.savefig("pump_loss.pdf")
         plt.show()
--- getter.py
+++ getter.py
@@ -1,25 +1,11 @@
 from gain_estimation import GainEstimation
 
 # Initialize the class object here, use default values or explicitly define the parameter
-simulation = GainEstimation(initial_pump_power=200, number_of_modes=1, losses_sled= 0)
+simulation = GainEstimation()
 
-# now one can easily access the parameter or methods defined in gain_estimation.py
 
-# e.g.
+# Plot power after crystal plot
+simulation.plot_amp_loss(modes=[1, 3], powers=[10, 50, 100, 200])
 
-# Just printing the set initial_pump_power
-# print(simulation.initial_pump_power)
-
-# Printing a list containing the values of the power after the crystal
-# print(simulation.calculate_power_after_crystal())
-
-# Plot a surface plot of input power output and length
-# simulation.plot_amp_loss_surface()
-
-# Plot normal plot
-# simulation.plot_amp_loss()
-
-result = simulation.get_power_after_crystal_list()
-#print(result)
-simulation.plot_amp_loss()
-#simulation.plot_amp_loss_surface()
+# Plot loss
+# simulation.plot_loss_pump(modes=[1,3], powers=[100, 200])
